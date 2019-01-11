dae2025a901bb4f0e20515b02372da737439b604
Lehchibi Gaël <lehchibi.gael@gmail.com>
2019-01-11 12:05:05 +0100

    First commit on Project.

---
+++ src/com/metaxiii/Main.java
@@ -0,0 +1,12 @@
+package com.metaxiii;
+
+import com.metaxiii.Game.Game;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Bonjour, Bienvenu dans ce Projet");
+        Game game = new Game();
+        game.initGame();
+    }
+}
