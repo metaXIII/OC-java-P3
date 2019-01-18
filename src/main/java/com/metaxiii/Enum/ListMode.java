6e5bf8c29a4e25a94b8a6099a5d62108c843c093
Lehchibi Gaël <lehchibi.gael@gmail.com>
2019-01-18 18:13:54 +0100

    Replace enum in good folder

---
+++ src/main/java/com/metaxiii/Enum/ListMode.java
@@ -0,0 +1,22 @@
+package com.metaxiii.Enum;
+
+public enum ListMode {
+    Challenger("1"),
+    Defenseur("2"),
+    Duel("3");
+
+    private String value;
+
+    ListMode(String value) {
+        this.value = value;
+    }
+
+    public static ListMode getMode(String el) {
+        for (ListMode element : ListMode.values()) {
+            if (el.equals(element.value))
+                return element;
+        }
+        return null;
+    }
+
+}
